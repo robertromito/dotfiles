09c3bea2e620af865bb29c58d9bf110020022ed7
Rob Romito <rob@robertromito.com>
2016-08-30 09:43:12 -0400

    Added basic .vimrc

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 ln -sfn ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -sfn ~/.dotfiles/bash/.bash_profile ~/.bash_profile
 ln -sfn ~/.dotfiles/bash/.bash_profile ~/.bashrc
+ln -sfn ~/.dotfiles/vim/.vimrc ~/.vimrc
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
