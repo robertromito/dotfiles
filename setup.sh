946ae2039eff41e33407b31468158d6288390d24
Rob Romito <rob@robertromito.com>
2016-09-07 10:01:32 -0400

    A little clean up on my setup file

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-ln -sfn ~/.dotfiles/git/.gitconfig ~/.gitconfig
-ln -sfn ~/.dotfiles/bash/.bash_profile ~/.bash_profile
-ln -sfn ~/.dotfiles/bash/.bash_profile ~/.bashrc
-ln -sfn ~/.dotfiles/vim/.vimrc ~/.vimrc
-ln -sfn ~/.dotfiles/ssh-add-my-keys ~/ssh-add-my-keys
+dotdir=~/.dotfiles
+ln_cmd='ln -sfn'
+
+$ln_cmd $dotdir/git/.gitconfig ~/.gitconfig
+$ln_cmd $dotdir/bash/.bash_profile ~/.bash_profile
+$ln_cmd $dotdir/bash/.bash_profile ~/.bashrc
+$ln_cmd $dotdir/vim/.vimrc ~/.vimrc
+
+$ln_cmd ~/.dotfiles/ssh-add-my-keys ~/ssh-add-my-keys
 chmod 700 ~/.dotfiles/ssh-add-my-keys
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
