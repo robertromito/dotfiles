2b5093de0745c5fe878382258352497375cc582c
Rob Romito <rob@robertromito.com>
2016-09-07 09:56:40 -0400

    Added script for adding my keys using ssh-add

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ ln -sfn ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -sfn ~/.dotfiles/bash/.bash_profile ~/.bash_profile
 ln -sfn ~/.dotfiles/bash/.bash_profile ~/.bashrc
 ln -sfn ~/.dotfiles/vim/.vimrc ~/.vimrc
+ln -sfn ~/.dotfiles/ssh-add-my-keys ~/ssh-add-my-keys
+chmod 700 ~/.dotfiles/ssh-add-my-keys
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
