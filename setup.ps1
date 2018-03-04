cfc644792d8cae8f312383e97f94f9ac8f7e1dbb
Rob Romito <rob@robertromito.com>
2018-03-04 10:22:54 -0500

    Added git config file

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,13 @@
 
-Write-Output "Setting up VSCode config files"
+Write-Output "VS Code config ..."
 $vscode_user_settings_path = "$env:APPDATA\Code\User"
 mkdir $vscode_user_settings_path
 ("settings.json", "keybindings.json") | ForEach-Object {
     Remove-Item $vscode_user_settings_path\$_
     cmd /c mklink $vscode_user_settings_path\$_ %cd%\vscode\$_
 }
+
+Write-Output "Git config ..."
+$git_config_dest = $env:HOME
+Remove-Item $git_config_dest\.gitconfig 
+cmd /c mklink $git_config_dest\.gitconfig %cd%\git\.gitconfig
