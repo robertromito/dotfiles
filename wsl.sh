12ef0b1d52cc98f8634a9633b4e7084d93351abb
Rob Romito <rob@robertromito.com>
2018-10-08 09:33:28 -0400

    Config for WSL

--- wsl.sh
+++ wsl.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Setup file for Windows Subsystem for Linux
-grep -q "export DOCKER_HOST=tcp://localhost:2375" ~/.bashrc || echo "export DOCKER_HOST=tcp://localhost:2375" >> ~/.bashrc
+grep -q ". dotfiles/bash/wsl.bash" ~/.bashrc || echo ". dotfiles/bash/wsl.bash" >> ~/.bashrc
+
+dotdir=$(pwd)
+ln_cmd='ln -sfn'
+
+$ln_cmd $dotdir/git/.gitconfig ~/.gitconfig
+$ln_cmd $dotdir/vim/.vimrc ~/.vimrc
