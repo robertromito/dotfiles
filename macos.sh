f4b4317df6caa666e5d6374bd2d759da79c6cdd2
Rob Romito <rob@robertromito.com>
2018-12-06 09:17:20 -0500

    Minor tune up for the neglected macOS

--- macos.sh
+++ macos.sh
@@ -4,6 +4,7 @@ dotdir=$(pwd)
 ln_cmd='ln -sfn'
 
 $ln_cmd $dotdir/git/.gitconfig ~/.gitconfig
+$ln_cmd $dotdir/git/.gitconfig-macos ~/.gitconfig-macos
 $ln_cmd $dotdir/bash/.bash_profile ~/.bash_profile
 $ln_cmd $dotdir/bash/.bash_profile ~/.bashrc
 $ln_cmd $dotdir/vim/.vimrc ~/.vimrc
