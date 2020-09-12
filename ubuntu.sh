55229810866664aed315499333be070fdd3226f5
Rob Romito <rob@robertromito.com>
2020-09-12 16:25:23 -0400

    Terminator config (#2)

--- ubuntu.sh
+++ ubuntu.sh
@@ -9,6 +9,10 @@ ln_cmd='ln -sfn'
 # git stuff
 $ln_cmd $dotdir/git/.gitconfig ~/.gitconfig
 
+# terminator
+mkdir -p ~/.config/terminator
+$ln_cmd $dotdir/terminator/config ~/.config/terminator/config
+
 sudo apt-get -y install powerline powerline-gitstatus
 mkdir -p ~/.config/powerline
 cp -r /usr/share/powerline/config_files/* ~/.config/powerline/
