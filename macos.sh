4410efc72c08a39c0810c2bda5ebfc77070fead6
Rob Romito <robert.romito@disneystreaming.com>
2021-03-17 13:11:42 -0400

    Tweaks to macos.sh

--- macos.sh
+++ macos.sh
@@ -5,11 +5,4 @@ ln_cmd='ln -sfn'
 
 $ln_cmd $dotdir/git/.gitconfig ~/.gitconfig
 $ln_cmd $dotdir/git/.gitconfig-macos ~/.gitconfig-macos
-$ln_cmd $dotdir/bash/.bash_profile ~/.bash_profile
-$ln_cmd $dotdir/bash/.bash_profile ~/.bashrc
-$ln_cmd $dotdir/vim/.vimrc ~/.vimrc
-$ln_cmd ~/Dropbox/pwiki ~/pwiki
-
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew update
-brew install git bash-completion bash-git-prompt tree wget
+$ln_cmd $dotdir/tmux.conf ~/.tmux.conf
