08c6647f153e6c49f18392fd4abd699b731b7b58
Rob Romito <rob@localhost.localdomain>
2024-09-01 12:19:41 -0400

    Wiping out old stuff

--- README.md
+++
@@ -1,3 +0,0 @@
-# My Workstation Config Files
-
-I use Windows, Ubuntu via WSL, and macOS regularly. This repo helps manage all my configs.
--- pop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Setup file for Windows Subsystem for Linux
-dotdir=$(pwd)
-
-ln_cmd='ln -sfn'
-
-$ln_cmd $dotdir/zsh/zsh.rc ~/.zshrc
-$ln_cmd $dotdir/git/.gitconfig ~/.gitconfig
-$ln_cmd $dotdir/tmux.conf ~/.tmux.conf
